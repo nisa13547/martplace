5a7a0b6b59a60afd7988f2dd63f8df7560b99e8a
nisa13547 <nisa13547@gmail.com>
2020-06-25 07:35:52 +0300

    added sections slider and products

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,27 @@
 $(function(){
 
+    $(".slider__inner-rate").rateYo({
+        rating: 4.6,
+        readOnly: true,
+        starWidth: "16px",
+        spacing: "3px",
+        
+                
+    });
+
+    $('.slider__inner').slick({
+        prevArrow: '<button class="slick-prev slick-arrow"></button>',
+        nextArrow: '<button class="slick-next slick-arrow"></button>',
+    });
+
+    $(".products__inner-rate").rateYo({
+        rating: 4.6,
+        readOnly: true,
+        starWidth: "12px",
+        spacing: "3px",
+        
+                
+    });
+
+
 });
